730f448e88e10224f082cbdac0fd68519727e0be
Christian Berendt <berendt@osism.tech>
2022-10-16 17:07:06 +0200

    Use material theme
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- source/conf.py
+++ source/conf.py
@@ -1,43 +1,36 @@
 def setup(app):
-    app.add_css_file('css/custom.css')
+    app.add_css_file("css/custom.css")
 
 
-extensions = [
-    'sphinx.ext.todo',
-    'sphinx_fontawesome'
-]
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'Timon User Guide'
-copyright = u'2022, OSISM GmbH'
-author = u'OSISM GmbH'
-version = u''
-release = u''
-language = 'en'
+extensions = ["sphinx.ext.todo", "sphinx_fontawesome"]
+source_suffix = ".rst"
+master_doc = "index"
+project = "Timon User Guide"
+copyright = "2022, OSISM GmbH"
+author = "OSISM GmbH"
+version = ""
+release = ""
+language = "en"
 exclude_patterns = []
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 todo_include_todos = True
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_material"
 html_show_sphinx = False
 html_show_sourcelink = False
 html_show_copyright = True
-htmlhelp_basename = 'documentation'
+htmlhelp_basename = "documentation"
 html_theme_options = {
-    'display_version': False,
-    'canonical_url': 'https://timontech.github.io/documentation/',
-    'style_external_links': True,
-    'logo_only': True,
-    'prev_next_buttons_location': None
+    "nav_title": "Timon User Guide",
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    "globaltoc_depth": 3,
+    "globaltoc_collapse": True,
 }
-html_context = {
-    'display_github': True,
-    'github_user': 'timontech',
-    'github_repo': 'documentation',
-    'github_version': 'main',
-    'conf_py_path': '/source/'
-}
-html_logo = 'images/logo.png'
-html_static_path = [
-    '_static'
-]
+html_context = {}
+html_logo = "images/logo.png"
+html_static_path = ["_static"]
 latex_elements = {}
+html_title = "Timon User Guide"
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
